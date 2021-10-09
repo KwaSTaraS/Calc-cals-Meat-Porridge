e1ecb6765f02d8b034aca917c6f37c00cd02e1a8
KwaSTaraS <kwas@MacBook-Pro-Taras.local>
2021-10-08 21:14:44 -0500

    Upload Calc calories meat & porridge html-css-js

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+// let gram = 0
+
+// function gr100(gram) {
+// 	let gram = 1
+// }
+// function gr200(gram) {
+// 	let gram = 2
+// }
+// function gr300(gram) {
+// 	let gram = 3
+// }
+var p
+let img
+document.addEventListener("DOMContentLoaded", function (event) {
+	p = document.getElementById('inputCals')
+	let = img = document.getElementById('img1')
+});
+
+function chickCalandImg() {
+	p.innerHTML = 190
+	img.src = "assets/images/chickF.jpg"
+}
+
+function porkCalandImg() {
+	p.innerHTML = 489
+	let img = document.getElementById('img1')
+	img.src = "assets/images/porkF.png"
+}
+
+function vealCalandImg() {
+	p.innerHTML = 131
+	let img = document.getElementById('img1')
+	img.src = "assets/images/vealF.png"
+}
+
+function riceCalandImg() {
+	p.innerHTML = 129
+	let img = document.getElementById('img1')
+	img.src = "assets/images/riceF.png"
+}
+
+function pastaCalandImg() {
+	p.innerHTML = 157
+	let img = document.getElementById('img1')
+	img.src = "assets/images/pastaF.png"
+}
+
+function buckWheatCalandImg() {
+	p.innerHTML = 343
+	let img = document.getElementById('img1')
+	img.src = "assets/images/buckweat.png"
+}
